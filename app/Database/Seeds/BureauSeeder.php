1e790f0b2b225f9c6b499279ac7cff622359792e
Titiss77 <Mathis.fcs.lav@gmail.com>
2026-01-06 21:19:57 +0100

    good

--- app/Database/Seeds/BureauSeeder.php
+++ app/Database/Seeds/BureauSeeder.php
@@ -18,15 +18,9 @@ class BureauSeeder extends Seeder
             ['nom' => 'Sandra PRIGENT', 'numTel' => '06 12 34 56 78', 'photo' => 'Sandra_PRIGENT.jpg', 'mail' => 'exemple@exemple.com'],
             ['nom' => 'Anne Catherine CAPITAINE', 'numTel' => '06 12 34 56 78', 'photo' => 'Anne_Catherine_CAPITAINE.jpg', 'mail' => 'exemple@exemple.com'],
             ['nom' => 'Sandra KERGOURLAY', 'numTel' => '06 12 34 56 78', 'photo' => 'Sandra_KERGOURLAY.jpg', 'mail' => 'exemple@exemple.com'],
-            ['nom' => 'Isabelle LECLERC', 'numTel' => '06 12 34 56 78', 'photo' => 'Isabelle_LECLERC.jpg', 'mail' => 'exemple@exemple.com'],
-            ['nom' => 'Lorette LE MOIGNE', 'numTel' => '06 12 34 56 78', 'photo' => 'Lorette_LE_MOIGNE.jpg', 'mail' => 'exemple@exemple.com'],
-            ['nom' => 'Christophe KERJEAN', 'numTel' => '06 12 34 56 78', 'photo' => 'Christophe_KERJEAN.jpg', 'mail' => 'exemple@exemple.com'],
-            ['nom' => 'Valérie THOMAS', 'numTel' => '06 12 34 56 78', 'photo' => 'Valérie_THOMAS.jpg', 'mail' => 'exemple@exemple.com'],
             ['nom' => 'Rodolphe LESPAGNOL', 'numTel' => '06 12 34 56 78', 'photo' => 'Rodolphe_LESPAGNOL.jpg', 'mail' => 'exemple@exemple.com'],
+            ['nom' => 'Valérie THOMAS', 'numTel' => '06 12 34 56 78', 'photo' => 'Valérie_THOMAS.jpg', 'mail' => 'exemple@exemple.com'],
             ['nom' => 'Mathis FRANCES', 'numTel' => '06 12 34 56 78', 'photo' => 'Mathis_FRANCES.jpg', 'mail' => 'exemple@exemple.com'],
-            ['nom' => 'Corrinne LE MEUR', 'numTel' => '06 12 34 56 78', 'photo' => 'Corrinne_LE_MEUR.jpg', 'mail' => 'exemple@exemple.com'],
-            ['nom' => 'Anne FRINAULT', 'numTel' => '06 12 34 56 78', 'photo' => 'Anne_FRINAULT.jpg', 'mail' => 'exemple@exemple.com'],
-            
             
         ];
         $db->table('membres')->insertBatch($membres);
@@ -34,49 +28,33 @@ class BureauSeeder extends Seeder
         // 2. Insertion des fonctions
         $fonctions = [
             ['titre' => 'President'], //1
-            ['titre' => 'Comptable'], //2
-            ['titre' => 'Secrétaire'],//3
-            ['titre' => 'Organisation compétitions'],//4
+            ['titre' => 'Vice-Président'], //2
+            ['titre' => 'Comptable'], //3
+            ['titre' => 'Secrétaire'],//4
             ['titre' => 'Partenariat / sponsoring'],//5
             ['titre' => 'Informatique'],//6
-            ['titre' => 'Convivialité du club'],//7
-            ['titre' => 'Eau libre'],//8
             
         ];
         $db->table('fonctions')->insertBatch($fonctions);
         
         $liaisons = [
             ['membre_id' => 1, 'fonction_id' => 1],
-            ['membre_id' => 1, 'fonction_id' => 4],
             
-            ['membre_id' => 2, 'fonction_id' => 1],
-            ['membre_id' => 2, 'fonction_id' => 7],
+            ['membre_id' => 2, 'fonction_id' => 2],
             
-            ['membre_id' => 3, 'fonction_id' => 2],
-            ['membre_id' => 3, 'fonction_id' => 8],
+            ['membre_id' => 3, 'fonction_id' => 3],
             
-            ['membre_id' => 4, 'fonction_id' => 2],
+            ['membre_id' => 4, 'fonction_id' => 3],
             
-            ['membre_id' => 5, 'fonction_id' => 3],
             ['membre_id' => 5, 'fonction_id' => 4],
 
-            ['membre_id' => 6, 'fonction_id' => 3],
+            ['membre_id' => 6, 'fonction_id' => 4],
 
-            ['membre_id' => 7, 'fonction_id' => 4],
+            ['membre_id' => 7, 'fonction_id' => 5],
+            
+            ['membre_id' => 8, 'fonction_id' => 5],
 
-            ['membre_id' => 8, 'fonction_id' => 4],
-
-            ['membre_id' => 9, 'fonction_id' => 4],
-
-            ['membre_id' => 10, 'fonction_id' => 5],
-
-            ['membre_id' => 11, 'fonction_id' => 5],
-
-            ['membre_id' => 12, 'fonction_id' => 6],
-
-            ['membre_id' => 13, 'fonction_id' => 7],
-
-            ['membre_id' => 14, 'fonction_id' => 8],
+            ['membre_id' => 9, 'fonction_id' => 6],
             
         ];
         $db->table('membre_fonction')->insertBatch($liaisons);
