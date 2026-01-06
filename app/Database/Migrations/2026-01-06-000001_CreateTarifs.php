7a4eac4a4766b533ea8e883f472b1cf0b90e8d4a
Titiss77 <Mathis.fcs.lav@gmail.com>
2026-01-06 18:45:49 +0100

    creates

---
+++ app/Database/Migrations/2026-01-06-000001_CreateTarifs.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateTarifs extends Migration
+{
+    public function up()
+    {
+        // Table Tarifs
+        $this->forge->addField([
+            'id'        => ['type' => 'INT', 'unsigned' => true, 'auto_increment' => true],
+            'categorie' => ['type' => 'VARCHAR', 'constraint' => 100],
+            'description' => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true],
+            'prix'      => ['type' => 'DECIMAL', 'constraint' => '10,2'],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('tarifs');
+    }
+
+    public function down() {
+        $this->forge->dropTable('tarifs');
+    }
+}
---
+++ app/Database/Migrations/2026-01-06-000003_CreatePostes.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreatePostes extends Migration
+{
+    public function up()
+    {
+        // Table Postes
+        $this->forge->addField([
+            'id'          => ['type' => 'INT', 'unsigned' => true, 'auto_increment' => true],
+            'libelle'         => ['type' => 'VARCHAR', 'constraint' => 100],
+            'mail' => ['type' => 'VARCHAR', 'constraint' => 255],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('postes');
+    }
+
+    public function down() {
+        $this->forge->dropTable('postes');
+    }
+}
