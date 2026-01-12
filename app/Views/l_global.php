7fd33a1da48729b1b2e660d2c200a31a66a6a7c7
Titiss77 <Mathis.fcs.lav@gmail.com>
2026-01-12 11:52:36 +0100

    cacher admin

--- app/Views/l_global.php
+++ app/Views/l_global.php
@@ -47,7 +47,7 @@
                 </a>
             </div>
             <p>&copy; <?= date('Y'); ?> <?= esc($general['nomClub']); ?>. Tous droits réservés.</p>
-            <p class="admin-link"><?= anchor('/login', '(Administration)'); ?></p>
+            <!--<p class="admin-link"><?= anchor('/login', '(Administration)'); ?></p>-->
         </footer>
         <!-- Fin de section -->
     </div>
